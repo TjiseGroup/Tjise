a3537f32eef806961acb4a1f3d89b11a7dda9f27
zwj242647 <zwj2426477795@foxmail.com>
2018-01-08 11:08:12 +0800

    body超出隐藏

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
 window.onload=function(){
-    document.body.setAttribute('style', 'margin: 0; border: 0; position: absolute; top: 0; width: 100%;');
+    document.body.setAttribute('style', 'margin: 0; border: 0; position: absolute; top: 0; width: 100%; overflow: hidden;');
     window.parent.postMessage(document.body.offsetHeight,'*');
 }
