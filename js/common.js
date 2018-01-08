55dfe7acf9e7ef85d4cabf9b84f03a7fafbf48ac
zwj242647 <zwj2426477795@foxmail.com>
2018-01-08 11:05:23 +0800

    高度自适应调整

--- js/common.js
+++ js/common.js
@@ -1,6 +1,4 @@
 window.onload=function(){
-    document.body.style.margin=0;
-    document.body.style.border=0;
+    document.body.setAttribute('style', 'margin: 0; border: 0; position: absolute; top: 0; width: 100%;');
     window.parent.postMessage(document.body.offsetHeight,'*');
-    console.log(document.body.offsetHeight)
 }
